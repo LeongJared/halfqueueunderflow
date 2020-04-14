58a614c66f07f0f7d1d30b3067cbe679324ce60f
LeongJared <57160549+LeongJared@users.noreply.github.com>
2020-04-13 20:29:15 -0700

    Update radarChart.js

--- radarChart.js
+++ radarChart.js
@@ -317,7 +317,7 @@ function RadarChart(id, data, options) {
 			//Bring back the hovered over blob
 			d3.select("." + data[d][0][areaName].replace(/\s+/g, ''))
 				.transition().duration(200)
-				.style("fill-opacity", 0.8);	
+				.style("fill-opacity", 0.7);
 	}
 
 	// on mouseout for the legend symbol
